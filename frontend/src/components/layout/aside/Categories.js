7ed085fc5418014be23afb7b677d4f149c4294ea
pawan <ps9502748@gmail.com>
2024-06-17 14:58:12 +0530

    working at pet categorie

--- frontend/src/components/layout/aside/Categories.js
+++ frontend/src/components/layout/aside/Categories.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import {
   getAllCategories,
@@ -17,10 +17,12 @@ import {
 } from "@mui/material";
 import { Typography } from "@material-ui/core";
 
-const Categories = ({ set_sub_cat_id, set_cat_id }) => {
+const Categories = () => {
+  const { category } = useParams();
   const dispatch = useDispatch();
   const Navigate = useNavigate();
   const { allcategroes } = useSelector((state) => state.allCategroe);
+  const [parent_slug, set_parent_slug] = useState(null);
 
   const { all_sub_categores } = useSelector((state) => state.sub_Categore);
   const [is_visiable_cat_list, set_is_visiable_cat_list] = useState(true);
@@ -32,19 +34,27 @@ const Categories = ({ set_sub_cat_id, set_cat_id }) => {
     dispatch(get_all_sub_categories());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (allcategroes) {
+      allcategroes &&
+        allcategroes.filter((item) => {
+          if (item.slug === category) set_parent_slug(null);
+        });
+    }
+  }, [category, allcategroes]);
+
   const toggleSubCategories = (index) => {
     const newVisibility = [...is_visiable_sub_cat_list];
     newVisibility[index] = !newVisibility[index];
     set_is_visiable__sub_cat_list(newVisibility);
   };
-  const currentPage = 1;
-  const price = [0, 1000];
-  const navigate_sub_cat_handler = (cat_id, sub_cat_id, slug) => {
-    updated_product_data(dispatch, currentPage, price, cat_id, sub_cat_id);
+  const navigate_sub_cat_handler = (cat_id, sub_cat_id, slug, cat_slug) => {
+    updated_product_data(dispatch, 1, [0, 1000], cat_id, sub_cat_id);
     Navigate(`/${slug}`);
+    set_parent_slug(cat_slug);
   };
   const navigate_cat_handler = (cat_id, slug) => {
-    updated_product_data(dispatch, currentPage, price, cat_id, "");
+    updated_product_data(dispatch, 1, [0, 1000], cat_id, "");
     Navigate(`/${slug}`);
   };
 
@@ -66,11 +76,16 @@ const Categories = ({ set_sub_cat_id, set_cat_id }) => {
           </Typography>
         </Box>
         <Box>
-          <Box style={{ padding: "50px 10px" }}>
+          <Box style={{ padding: "10px 10px" }}>
             <List>
               {allcategroes &&
                 allcategroes
-                  .filter((item) => item.category_status === "Active")
+                  .filter(
+                    (item) =>
+                      item.category_status === "Active" &&
+                      item.slug ===
+                        (parent_slug !== null ? parent_slug : category)
+                  )
                   .map((item, i) => (
                     <ListItem
                       sx={{ display: "block !important" }}
@@ -84,27 +99,19 @@ const Categories = ({ set_sub_cat_id, set_cat_id }) => {
                           }
                           primary={item.name}
                         />
-                                             <span
-                        style={{ cursor: "pointer" }}
-                        className="col-md-3"
-                        onClick={() => toggleSubCategories(i)}
-                      >
-                        {is_visiable_sub_cat_list[i] ? (
-                          <IoIosArrowForward />
-                        ) : (
-                          <IoIosArrowDown />
-                        )}
-                      </span>
+                        <span
+                          style={{ cursor: "pointer" }}
+                          className="col-md-3"
+                          onClick={() => toggleSubCategories(i)}
+                        >
+                          {is_visiable_sub_cat_list[i] ? (
+                            <IoIosArrowForward />
+                          ) : (
+                            <IoIosArrowDown />
+                          )}
+                        </span>
                       </ListItemButton>
-                      <Box
-                      
-                        // className={
-                        //   visible === i
-                        //     ? "child-navlist list-active"
-                        //     : "child-navlist "
-                        // }
-                        component={"div"}
-                      >
+                      <Box component={"div"}>
                         <List sx={{ paddingLeft: "30px !important" }}>
                           {all_sub_categores &&
                             all_sub_categores
@@ -115,13 +122,13 @@ const Categories = ({ set_sub_cat_id, set_cat_id }) => {
                               )
                               .map((subItem, i) => (
                                 <ListItem sx={{ padding: 0 }}>
-                                  {/* <img src="/box.webp" /> */}
                                   <ListItemText
                                     onClick={() =>
                                       navigate_sub_cat_handler(
                                         item._id,
                                         subItem._id,
-                                        subItem.slug
+                                        subItem.slug,
+                                        item.slug
                                       )
                                     }
                                     primary={subItem.name}
